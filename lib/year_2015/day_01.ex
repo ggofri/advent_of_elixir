1fdd432d2addb79f18014bbb30a012e87422ba7f
Julian Ramirez <ignaciojulianramirez@gmail.com>
2025-07-14 22:08:21 -0300

    chore: format day 1-2015

--- lib/year_2015/day_01.ex
+++ lib/year_2015/day_01.ex
@@ -3,21 +3,24 @@ defmodule Year2015.Day01 do
   Year 2015, Day 01, Challenge 1
   """
 
-  def solve_01 input do
+  def solve_01(input) do
     input
-    |> String.graphemes
-    |> Enum.reduce(0, fn char, acc -> char == "(" && acc + 1 || acc - 1 end)
+    |> String.graphemes()
+    |> Enum.reduce(0, fn char, acc -> (char == "(" && acc + 1) || acc - 1 end)
   end
 
   defp find_basement(_input, level, iterations) when level == -1 do
     iterations
   end
+
   defp find_basement(input, _level, _iterations) when length(input) == 0 do
     0
   end
+
   defp find_basement(input, level, iterations) do
     [first | rest] = input
     new_iterations = iterations + 1
+
     case first do
       "(" -> find_basement(rest, level + 1, new_iterations)
       _ -> find_basement(rest, level - 1, new_iterations)
